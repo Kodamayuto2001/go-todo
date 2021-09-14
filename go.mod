446a591ee3aea3539f8c8168fc7cd4252da2d1d0
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2021-09-15 08:30:58 +0900

    修正

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module go-api1
 
 go 1.13
 
-require github.com/google/uuid v1.3.0 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.4 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
+	"database/sql"
+	"os"
 	
 	"github.com/google/uuid"
 	"github.com/gin-gonic/gin"
@@ -33,12 +36,41 @@ type Boards struct {
 	Name	string	`json:"name"`
 }
 
-func AddBoard(board *Boards) {
+func AddBoard(board *Boards) string {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(err)
+		return "godotenv err!"
 	}
 	db, err := sql.Open("mysql",os.Getenv("DB_USER")+":"+os.Getenv("DB_PASSWORD")+"@/"+os.Getenv("DB_NAME"))
+	if err != nil {
+		log.Fatal(err)
+		return "db open err!"
+	}
+	defer db.Close()
+
+	ins, err := db.Prepare("INSERT INTO boards(name) VALUES(?)")
+	if err != nil {
+		log.Fatal(err)
+		return "db.Prepare err!"
+	}
+
+	ins.Exec(board.Name)
+
+	id, err := db.Query("SELECT id FROM boards")
+	if err != nil {
+		log.Fatal(err)
+		return "db select err!"
+	}
+	defer id.Close()
+
+	err = id.Scan(board.ID)
+	if err != nil {
+		log.Fatal(err)
+		return "id.Scan err!"
+	}
+
+	return "nil"
 }
 
 func main() {
@@ -50,22 +82,28 @@ func main() {
 		var boards Boards
 		err := c.ShouldBindJSON(&(boards.Name))
 		if err != nil {
-			c.JSON(http.Status.BadRequest,gin.H{
+			c.JSON(http.StatusBadRequest,gin.H{
 				"error": err.Error(),
 			})
 			return
 		}
 
+		message := AddBoard(&boards)
+		if message != "nil" {
+			fmt.Println(message)
+		}
 
 		c.JSON(
 			http.StatusOK,
 			gin.H{
-				"message":"Board is created"
-			}
+				"message":"Board is created",
+			},
 		)
 	})
 
-	engine.GET("/api/v1/users/:id/boards",func(c *gin.Context){
+	// engine.GET("/api/v1/users/:id/boards",func(c *gin.Context){
 
-	})
+	// })
+
+	engine.Run(":3000")
 }
