f8865c9d5ef99006cdab46d458b988947a67c4d6
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2021-09-15 07:54:15 +0900

    修正

--- main.go
+++ main.go
@@ -28,10 +28,42 @@ func makeUUID() string {
 //	/api/v1/users/:id/boardにGETしてきた場合、
 //	DBからユーザーIDに合致する全ボードを取得し、Jsonで返す
 
+type Boards struct {
+	ID		int		`json:"id"`
+	Name	string	`json:"name"`
+}
+
+func AddBoard(board *Boards) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, err := sql.Open("mysql",os.Getenv("DB_USER")+":"+os.Getenv("DB_PASSWORD")+"@/"+os.Getenv("DB_NAME"))
+}
+
 func main() {
 	makeUUID()
 
 	engine := gin.Default()
+
+	engine.POST("/api/v1/users/:id/boards",func(c *gin.Context){
+		var boards Boards
+		err := c.ShouldBindJSON(&(boards.Name))
+		if err != nil {
+			c.JSON(http.Status.BadRequest,gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+
+		c.JSON(
+			http.StatusOK,
+			gin.H{
+				"message":"Board is created"
+			}
+		)
+	})
 
 	engine.GET("/api/v1/users/:id/boards",func(c *gin.Context){
 
